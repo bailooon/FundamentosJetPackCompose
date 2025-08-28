c477e82f90dd1ea5f59109a5af00ace690683b67
bailooon <matheus@bailon.com.br>
2025-08-28 16:24:00 -0300

    Anotação de Preview - Ajuste de Modifier.fillMaxSize()

--- app/src/main/java/bailooon/com/github/fundamentosjetpackcompose/MainActivity.kt
+++ app/src/main/java/bailooon/com/github/fundamentosjetpackcompose/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
 fun TextoCustomizado(texto: String, modifier: Modifier = Modifier) {
     Text(
         text = texto,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
     )
 }
 
